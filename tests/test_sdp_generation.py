bcc98d3e24a5b436348b30ad508ff8b23424a344
Eckschi <me@eckschi.net>
2026-02-13 19:41:32 +0100

    generator test pt1

--- tests/test_sdp_generation.py
+++ tests/test_sdp_generation.py
@@ -1,5 +1,8 @@
 import unittest
-from . import context
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 from sipapy.SdpBody import SdpBody
 from sipapy.SdpOrigin import SdpOrigin
 from sipapy.SdpMediaDescription import SdpMediaDescription, MediaType
